fa0fb2f047026ac1a70363a30bfee21004f71e74
Matěj <vas@email.com>
2018-05-03 12:56:11 +0200

    H O T O V O )))

--- index.php
+++ index.php
@@ -5,18 +5,26 @@
     <title></title>
   </head>
   <body>
-<h1>Osobní profil</h1>
-
-<form action="submit.php" method="post">
-  <label for="jmeno">Jméno</label>
-  <input id="jmeno" name='jmeno' placeholder="Jan" /> <br />
-  <label for="primeni">Příjmění</label>
-  <input id="primeni" name='primeni' placeholder="Novák" /> <br />
-  <label for="age">Věk</label>
-  <input id="age" min="1" max="100" type="number" name='age'  /> <hr />
-
-    <input name='submit' type="submit" value="Odeslat" />
-
+<h1>Hra</h1>
+<?php
+    $KCGPU = 10 ; // index spotřeb.
+    $KCCPU = 10 ; // index spotřeb.
+    $KCRAM = 16 ; // GB
+    $KCHDD = 40 ; // GB
+//------------------------------------------------------------------------------
+    $myGPU = 10 ; // Moje GPU koupený z Alzy.cz. (TAKZE LEDŽIT)
+    $myCPU = 10 ; // Moje CPU co jsem koupil včera u čongů.
+    $myRAM = 16 ; // Tyhle RAMky jsem vzal ze školy. (GIGA)
+    $myHDD = 60 ; // HDD jsem si vzal od dědy (GIGABAJTŮ).
+?>
+<?php
+   if (($KCGPU <= $myGPU)&&($KCCPU <= $myCPU)&&($KCRAM <= $myRAM)&&($KCHDD <= $myHDD)) {
+      echo "V pohodě. tu hru rozjedeš";
+   }
+   else {
+     echo "Máš smůlu. zahraj si radši Tetris";
+   }
+ ?>
 
   </body>
 </html>
